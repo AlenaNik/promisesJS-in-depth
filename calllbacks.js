45aa7c6d2efc820a3cb36c9ee31843b51eccd696
Alena <alena.nik.digital@gmail.com>
2019-09-01 18:19:08 +0200

    native promisify readFile method

---
+++ calllbacks.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const util = require("util");
+
+const readFile = util.promisify(fs.readFile);
+
+readFile(__filename, "utf8")
+.then(contents => console.log(contents),
+    error => {
+    console.error(error);
+    }
+);
