02f6a03241fbd1880d8063dca642b6868700595f
Alena <alena.nik.digital@gmail.com>
2019-09-01 17:11:15 +0200

    how to list films with nodemon

--- index.js
+++ index.js
@@ -13,7 +13,13 @@ app.use(cors());
 
 const API_URL = "https://starwars.egghead.training/";
 const responsePromise = fetch(API_URL + 'films');
-console.log(responsePromise);
+responsePromise.then(response => {
+    return response.json();
+}).then(films => {
+    const filmTitles= films.map(film => `${film.episode_id}. ${film.title}`)
+        .join("\n");
+    console.log(filmTitles);
+});
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
